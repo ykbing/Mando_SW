594936cf3357c7808392f2a500413e7af63ca35c
ykbing <101696361+ykbing@users.noreply.github.com>
2022-12-01 01:10:16 +0900

    Add files via upload

---
+++ 20221130_BPS/2022113.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int BPS_value, B_value;
+    float result_msec, time, BPS, B_input, B;
+
+    printf("BPS를 입력해주세요: ");
+    scanf("%d", &BPS_value);
+
+    printf("통신 Byte를 입력해주세요: ");
+    scanf("%d", &B_value);
+
+    // 전송 속도(S) = 데이터의 양(A) / 전송 시간(T)
+
+    // msec <- sec 변환
+    BPS = BPS_value / 1000;
+    B = B_value;
+
+    result_msec = 1 / BPS;
+    B_input = result_msec * B;
+
+    printf("1Byte 당 통신 소요시간: %0.3f msec \n", result_msec);
+    printf("입력 바이트에 맞는 통신 시간: %0.3f msec", B_input);
+}
