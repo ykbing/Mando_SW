f5943f8aca4f61531b26c484e15b8112e968f578
ykbing <101696361+ykbing@users.noreply.github.com>
2022-12-11 21:40:56 +0900

    Update dead_zone.ino

--- 20221206_DeadZone/dead_zone/dead_zone.ino
+++ 20221206_DeadZone/dead_zone/dead_zone.ino
@@ -1,3 +1,9 @@
+/*
+  MsTimer2 is a small and very easy to use library to interface Timer2 with
+  humans. It's called MsTimer2 because it "hardcodes" a resolution of 1
+  millisecond on timer2
+  For Details see: http://www.arduino.cc/playground/Main/MsTimer2
+*/
 #include <MsTimer2.h>
 
 #define encodrpinA1 2
@@ -5,24 +11,54 @@
 #define INA 4
 #define PWM 5
 #define check_pin 6
+// Switch on LED on and off each half second
 
 volatile int32_t encoderPos = 0;
+volatile int32_t encoderPos_old = 0;
+int32_t target_Pos = 0;
+int32_t pos_error = 0;
+int32_t pos_error_old = 0;
+int32_t pos_error_d = 0;
 
-long pid_pwm = 0.0;
-int deadzone_count = 0;
-int old_deadzone_count = 0;
+float p = 0.8;
+float pd = 1.5;
+float pi = 0.2;
+
+int pid_pwm = 0;
+
+long pos_error_sum = 0.0;
+
+#if ARDUINO >= 100
+const int led_pin = LED_BUILTIN;  // 1.0 built in LED pin var
+#else
+const int led_pin = 13;     // default to pin 13
+#endif
 
 void Timer()
 {
-  deadzone_count = abs(encoderPos) <= 2 ? deadzone_count+=1 : deadzone_count;
+  static boolean output = HIGH;
   
-  if(old_deadzone_count != deadzone_count)
+  digitalWrite(led_pin, output);
+  digitalWrite(check_pin, output);
+  output = !output;
+  pos_error = target_Pos - encoderPos;
+  pos_error_d = pos_error - pos_error_old;
+  encoderPos_old = encoderPos;
+  pos_error_sum += pos_error;
+  pos_error_sum = (pos_error_sum > 50) ? 50 : pos_error_sum;
+  pos_error_sum = (pos_error_sum < -50) ? -50 : pos_error_sum;
+
+  if(abs(pos_error) <= 1)
   {
-      pid_pwm++; 
+    pos_error_sum = 0;
   }
-  
-  old_deadzone_count = deadzone_count;
-  
+  pos_error_old = pos_error;
+
+//  pid_pwm = pi*pos_error_sum + p*pos_error + pd*pos_error_d;
+
+//  pid_pwm = (pos_error > (double)target_Pos * 0.5) ? 255 : pid_pwm;  Feedfoward control
+   pid_pwm = pid_pwm > 255 ? 255 : pid_pwm;
+
   if(pid_pwm > 0)
   {
     motor_control(1, pid_pwm);
@@ -31,6 +67,7 @@ void Timer()
     motor_control(-1, pid_pwm);
   }
   
+  digitalWrite(check_pin, LOW);
 }
 
 void interrupt_setup(void)
@@ -40,10 +77,9 @@ void interrupt_setup(void)
   pinMode(encodrpinB1, INPUT_PULLUP);             
   attachInterrupt(0, encoder, RISING);                               
 }
-
+  
 void motor_control(int direc, int pwm)
 {
-  pwm = (pwm>=255) ? 255 : pwm;
   switch(direc)
   {
     case 1: digitalWrite(INA, HIGH);
@@ -68,20 +104,25 @@ void encoder()
   }
 }
 
-void setup() 
+void setup()
 {
+  pinMode(led_pin, OUTPUT);
+  pinMode(check_pin, OUTPUT);
   MsTimer2::set(50, Timer); // 500ms period
   MsTimer2::start();
   interrupt_setup();
   Serial.begin(9600);
+  target_Pos = 500;
 }
 
-void loop() 
-{
-  Serial.print("encoderPos : ");
-  Serial.print(encoderPos);
-  Serial.print("  deadzone_count : ");
-  Serial.print(deadzone_count);
-  Serial.print("  pwm : ");
-  Serial.println(pid_pwm);
+void loop()
+{   
+    Serial.print(pos_error_d);
+    Serial.print(",");
+    //Serial.print("  pid_pwm: ");
+    Serial.println(pid_pwm);
+
+    pid_pwm++;
+    delay(1000);
+//  motor_control(-1, 23);   // foward 23  , backward 20
 }
